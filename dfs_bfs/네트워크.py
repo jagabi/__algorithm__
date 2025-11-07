9fb1e6af87dbabf1db7af5b8e377270df810fa16
jagabi <yundong0913@gmail.com>
2025-11-07 17:01:19 +0900

    feat: dfsbfs

---
+++ dfs_bfs/네트워크.py
@@ -0,0 +1,25 @@
+def bfs(n, computers, com, visited):
+    queue = []
+    queue.append(com)
+    visited.append(com)
+    result = [com]
+    while queue:
+        current = queue.pop(0)
+        for idx, computer in enumerate(computers[current]):
+            if idx not in visited and computer == 1:
+                queue.append(idx)
+                visited.append(idx)
+                result.append(idx)            
+    
+    return result
+        
+    
+def solution(n, computers):
+    visited = []
+    result = []
+    for com in range(len(computers)):
+        if com in visited:
+            continue
+        result.append(bfs(n, computers, com, visited))
+
+    return len(result)
